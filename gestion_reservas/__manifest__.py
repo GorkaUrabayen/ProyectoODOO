4ff24a8403d4e1d2bdf1a38f27e3f38444ef1422
Izan <iramoscerv1@educacion.navarra.es>
2025-02-11 10:28:44 +0100

    Seguridad y permisos sobre los modelos
    Fix #17

--- gestion_reservas/__manifest__.py
+++ gestion_reservas/__manifest__.py
@@ -4,11 +4,9 @@
     'summary': 'Módulo de reservas de mazmorras y princesas',
     'author': 'Gorka Urabaye, Izan Ramos y Paula Iturbide',
     'depends': ['base','account'],
-    #Data por definir
     'data': [
-        'security/ir.model.access.csv',  # Permisos de acceso a los modelos
-        'views/mi_modelo_views.xml',  # Definiciones de vistas
-        'data/mi_modelo_data.xml',  # Datos predefinidos
+        'security/ir.model.access.csv',
+        'security/reservas_security.xml',
     ],
     'installable': True,
     'application': True,
