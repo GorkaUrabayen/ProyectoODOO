ce8a5c84871db6812abd6b4a6cde47762bc033c5
Izan <iramoscerv1@educacion.navarra.es>
2025-02-18 10:18:19 +0100

    Manifest

--- gestion_reservas/__manifest__.py
+++ gestion_reservas/__manifest__.py
@@ -1,12 +1,20 @@
 {
-    'name': '',
+    'name': 'Gestión de Mazmorras y Reservas',
     'version': '1.0',
-    'summary': 'Módulo de reservas de mazmorras y princesas',
+    'summary': 'Gestiona mazmorras, reservas y princesas',
+    'description': """
+        Este módulo permite gestionar reservas de mazmorras, 
+        incluyendo clientes VIP, princesas y servicios.
+    """,
+    'category': 'Services',
     'author': 'Gorka Urabayen, Izan Ramos y Paula Iturbide',
     'depends': ['base','account'],
+    'images': ['static/description/icon.png'],
     'data': [
         'security/ir.model.access.csv',
         'security/reservas_security.xml',
+        'views/menu.xml'
+
     ],
     'installable': True,
     'application': True,
